14ddaabf67bf307e484ee049a6ed95038743e1db
Devin Coster <doctorangler91@gmail.com>
2025-11-18 19:07:41 -0500

    Create file structure and added all necessary cpp/h files that I believe will be needed to create this Terminal Messenger.

---
+++ src/client/client.cpp
@@ -0,0 +1,5 @@
+//
+// Created by docto on 11/18/2025.
+//
+
+#include "client.h"
---
+++ src/client/client.h
@@ -0,0 +1,14 @@
+//
+// Created by docto on 11/18/2025.
+//
+
+#ifndef NETWORK_MESSENGER_CLIENT_H
+#define NETWORK_MESSENGER_CLIENT_H
+
+
+class client
+{
+};
+
+
+#endif //NETWORK_MESSENGER_CLIENT_H
